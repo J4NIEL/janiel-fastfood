d591da62ae662d3f375da77a935ba3c11a187473
J4NIEL <70277881+J4NIEL@users.noreply.github.com>
2025-01-19 18:26:25 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'JANIEL'
+description 'Janiel Fastfood'
+version '1.0.0'
+
+shared_script 'config.lua' -- Config dosyası
+client_script 'client.lua'
+server_script 'server.lua'
