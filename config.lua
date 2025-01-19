65a9e63ce6cb565f67ba5257d86714dbe81dbd0a
J4NIEL <70277881+J4NIEL@users.noreply.github.com>
2025-01-19 18:26:06 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.Jobs = { "emlak", "emlak1", "emlak2" }
+
+Config.Locations = {
+    vector3(-1837.23, -1214.50, 13.14),
+    vector3(-1032.56, -421.57, 39.62),
+    vector3(-267.00, -961.71, 31.22)
+}
+
+Config.MenuItems = {
+    { item = "milkshake", header = "🥤 Milkshake", txt = "Soğuk ve lezzetli milkshake", event = "menu:selectFood", args = "milkshake" },
+    { item = "bscoffee", header = "☕ Kahve", txt = "Sıcak bir bardak kahve", event = "menu:selectFood", args = "bscoffee" },
+    { item = "rimjob", header = "🍩 Donut", txt = "Tatlı bir donut", event = "menu:selectFood", args = "rimjob" },
+    { item = "bleeder", header = "🍔 Hamburger", txt = "Lezzetli bir hamburger", event = "menu:selectFood", args = "bleeder" },
+    { item = "pizza", header = "🍕 Pizza", txt = "Taze pişmiş pizza", event = "menu:selectFood", args = "pizza" },
+    { item = "noodlebowl", header = "🍜 Noodle", txt = "Lezzetli bir noodle kasesi", event = "menu:selectFood", args = "noodlebowl" },
+    { item = "popssundae", header = "🍨 Dondurma", txt = "Tatlı ve soğuk dondurma", event = "menu:selectFood", args = "popssundae" },
+    { item = "popssprite", header = "🥤 Kola", txt = "Soğuk bir kutu kola", event = "menu:selectFood", args = "popssprite" },
+    { item = "hotdog", header = "🌭 Sosisli", txt = "Sıcak bir sosisli", event = "menu:selectFood", args = "hotdog" }
+}
